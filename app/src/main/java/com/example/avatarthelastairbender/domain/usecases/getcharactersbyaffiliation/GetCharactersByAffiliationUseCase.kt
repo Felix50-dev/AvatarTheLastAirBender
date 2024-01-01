8de4ab2f99af261f7afadf8fbad692b8d6b0b7dc
Felix50-dev <mwatufelix1@gmail.com>
2024-01-01 22:59:27 +0300

    Fixed viewmodel

--- app/src/main/java/com/example/avatarthelastairbender/domain/usecases/getcharactersbyaffiliation/GetCharactersByAffiliationUseCase.kt
+++ app/src/main/java/com/example/avatarthelastairbender/domain/usecases/getcharactersbyaffiliation/GetCharactersByAffiliationUseCase.kt
@@ -1,29 +1,19 @@
 package com.example.avatarthelastairbender.domain.usecases.getcharactersbyaffiliation
 
-import com.example.avatarthelastairbender.common.Resource
 import com.example.avatarthelastairbender.domain.model.CharacterAffiliation
 import com.example.avatarthelastairbender.domain.model.toCharacterAffiliation
 import com.example.avatarthelastairbender.domain.repository.AvatarRepository
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
-import retrofit2.HttpException
-import java.io.IOException
 import javax.inject.Inject
 
 class GetCharactersByAffiliationUseCase @Inject constructor(
     private val repository: AvatarRepository
 ) {
 
-    operator fun invoke(nation: String): Flow<Resource<List<CharacterAffiliation>>> = flow {
-        try {
-            emit(Resource.Loading())
-            val charactersByAffiliation = repository.getCharactersByAffiliation(nation)
-            emit(Resource.Success(charactersByAffiliation.map { it.toCharacterAffiliation() }))
-        } catch (e: HttpException) {
-            emit(Resource.Error(e.localizedMessage ?: "unexpected error"))
-        } catch (e: IOException) {
-            emit(Resource.Error("couldn't reach server. Check your internet connection"))
-        }
+    operator fun invoke(nation: String): Flow<List<CharacterAffiliation>> = flow {
+        val charactersByAffiliation = repository.getCharactersByAffiliation(nation)
+        emit(charactersByAffiliation.map { it.toCharacterAffiliation() })
     }
 
 }
--- app/src/main/java/com/example/avatarthelastairbender/presentation/avatar_list/AvatarListViewModel.kt
+++ app/src/main/java/com/example/avatarthelastairbender/presentation/avatar_list/AvatarListViewModel.kt
@@ -4,39 +4,63 @@ import androidx.compose.runtime.State
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.example.avatarthelastairbender.common.Resource
+import com.example.avatarthelastairbender.domain.model.Avatar
+import com.example.avatarthelastairbender.domain.model.CharacterAffiliation
 import com.example.avatarthelastairbender.domain.usecases.getavatars.GetAvatarsUseCase
+import com.example.avatarthelastairbender.domain.usecases.getcharactersbyaffiliation.GetCharactersByAffiliationUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.launchIn
-import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class AvatarListViewModel @Inject constructor(
     private val getCAvatarsUseCase: GetAvatarsUseCase,
+    private val getCharactersByAffiliationUseCase: GetCharactersByAffiliationUseCase,
 ) : ViewModel() {
 
-    private val _state = mutableStateOf(AvatarListState())
-    val state: State<AvatarListState> = _state
+    private val _state = mutableStateOf(MainScreenListViewState())
+    val state: State<MainScreenListViewState> = _state
 
     init {
-        getAvatars()
-    }
+        viewModelScope.launch {
+            val earthBenders = getCharactersByAffiliationUseCase("earth")
+            val waterBenders = getCharactersByAffiliationUseCase("water")
+            val fireBenders = getCharactersByAffiliationUseCase("fire")
+            val avatars = getCAvatarsUseCase()
 
-
-    private fun getAvatars() {
-        getCAvatarsUseCase().onEach { result ->
-            when (result) {
-                is Resource.Success -> {
-                    _state.value = AvatarListState(avatars = result.data)
-                }
-                is Resource.Error -> {
-                    _state.value = AvatarListState(error = result.message ?: "Unexpected error")
-                }
-                is Resource.Loading -> {
-                    _state.value = AvatarListState(isLoading = true)
-                }
-            }
-        }.launchIn(viewModelScope)
+            combine(
+                earthBenders,
+                waterBenders,
+                fireBenders,
+                avatars
+            ) { earthBendersList, waterBendersList, fireBendersList, avatarsList ->
+                MainScreenListViewState(
+                    earthBendersList,
+                    waterBendersList,
+                    fireBendersList,
+                    avatarsList
+                )
+            }.collect { _state.value = it }
+        }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+data class MainScreenListViewState(
+    val earthBendersList: List<CharacterAffiliation> = emptyList(),
+    val waterBendersList: List<CharacterAffiliation> = emptyList(),
+    val fireBendersList: List<CharacterAffiliation> = emptyList(),
+    val avatarsList: List<Avatar> = emptyList()
+)
